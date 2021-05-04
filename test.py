317b76af4b0732148f841f67cc7dfc0507cbedd8
DorianGriffin <dorianagriffin@gmail.com>
2021-05-04 15:49:31 -0500

    libraries

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-#good code goes here
-#free all birds today
-print('Hello there, my dudes')
+import matplotlib.pyplot as plt
+import numpy
+import scipy
+import pandas
+import pygame
+import pip
+pip.main(['install','seaborn'])
+
+
