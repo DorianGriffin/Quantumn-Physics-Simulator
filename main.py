f4ae4cc98b3dd3cf94b3464d8648d0df012dfb69
Dorian Griffin <dorianagriffin@gmail.com>
2021-06-05 11:02:43 -0500

    Figured out graph 1

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 # with higher pressure/temperature comes higer particle acceleration
+#
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import scipy
-import pandas
+ # import pandas
 import pygame
 import tensorflow
 import pip
-pip.main(['install','seaborn'])
-
-
+# pip.main(['install','seaborn'])
+Gas_constant = 8.3145 
+temp = 220
+n = 500
+volume = np.array(range(10))
 #will read a csv file with volume, temperatutre, gas constant, number of moles
+#def test_func(volume, temp, n):
+P = (n * (Gas_constant * 0.01) * temp)/(volume)
+plt.plot(volume,P)
+plt.title('Relationship between Volume and Pressure')
+plt.xlabel('x Axis: Volume')
+plt.ylabel('y Axis: Pressure')
+plt.grid(alpha=.4,linestyle='-- ')
+plt.show()
+    #print(P)
+    #calculate the wave propogation based upon the pressure of a given volume; we will visualize it
 
-def test_func(volume, temp, n,G):
+#testing the gas pressure equation, super basic
+
+
+#test_func(np.array(range(10000)), 220, 500)
+
+
 
 
 
